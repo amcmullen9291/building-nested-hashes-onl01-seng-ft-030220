6f8dd746b21a7636a2f2d3dd96e4e2c682ff36eb
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-09 04:13:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,14 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name of "Lord Montague",
-     :age of "53"},
+      :patriarch => {:name=>"Lord Montague",
+     :age=>"53"},
    },
       :matriarch => {
-      :name of "Lady Montague",       },
-      :age of "54"
-      :hero => {:name of "Romeo",:age of "15",:status of "alive" },
-      :hero_friends => []
+      :name=>"Lady Montague",       },
+      :age=>"54"
+      :hero => {:name=>"Romeo",:age=>"15",:status=>"alive" },
+      :hero_friends=> []
    }, 
    :capulet => {
       :patriarch => {
